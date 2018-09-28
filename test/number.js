ab8b0370828192e2fefc2fae1858c6429ca259b7
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-09-28 10:00:07 +0800

    fix sa.number output NaN when input numeric string issue.(close #12)

--- test/number.js
+++ test/number.js
@@ -6,6 +6,10 @@ test('# sa.number should output original number when input number', t => {
   t.is(sa.number(1), 1)
 })
 
+test('# sa.number output number when input numeric string', t => {
+  t.is(sa.number('1'), 1)
+})
+
 test('# sa.number should output NaN when input is undefined, null, array, object or function', t => {
   t.is(sa.number(null), NaN)
   t.is(sa.number(undefined), NaN)
