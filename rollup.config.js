b2d1106b7726a8f4dec011b390cf6823e5522866
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-10-03 16:24:41 +0800

    build: upgrade mili

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import { compiler } from './build.config'
 
-export default [{
+
+export default compiler({
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
-
-  plugins: [
-    resolve(),
-    babel({
-      babelrc: false,
-      presets: [
-        ["env", {
-          modules: false
-        }],
-      ],
-      plugins: ["transform-object-rest-spread", "external-helpers"],
-      // runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }),
-  ],
-}];
+  output: { file: 'dist/bundle.js' },
+})
